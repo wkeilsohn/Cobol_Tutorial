5213e65ddf81cf671a1ea414b1662e16ab23ba04
user <user@NOTER023.tkh-staff.local>
2025-05-29 15:21:04 +0900

    Lesson 2 Update

--- Lesson_2/Lesson_2.cob
+++ Lesson_2/Lesson_2.cob
@@ -34,5 +34,47 @@
        move "123Bob Smith           12211974" To Customer.
        display CustName
        display MOB "/" DOB "/" YOB.
+       move zero to SampleData.
+       display SampleData.
+       move space to SampleData.
+       move HIGH-VALUE to SampleData.
+       display SampleData.
+       move LOW-VALUE to SampleData.
+       display SampleData.
+       move quote to SampleData.
+       display SampleData.
+       move all "2" to SampleData.
+       display SampleData.
+
+       ADD Num1 to Num2 giving Ans.
+       display Ans.
+       SUBTRACT Num1 from Num2 giving Ans.
+       display Ans.
+       MULTIPLY Num1 by Num2 giving Ans.
+       display Ans.
+       DIVIDE Num1 into Num2 giving Ans.
+       display Ans.
+
+       DIVIDE Num1 into Num2 giving Ans remainder Rem.
+       display "Remainder" Rem.
+
+       Add Num1, Num2 to Num3 Giving Ans.
+       Add Num1, Num2, Num3 giving Ans.
+       display Ans.
+
+       Compute Ans = Num1 + Num2.
+       Compute Ans = Num1 - Num2.
+       Compute Ans = Num1 * Num2.
+       Compute Ans = Num1 / Num2.
+       display Ans.
+
+       Compute Ans = Num1 ** 2.
+       display Ans.
+       Compute Ans = ( 3 + 5 ) * 5.
+       display Ans.       
+       Compute Ans = 3 + 5 * 5.
+       display Ans.       
+       Compute Ans rounded = 3.0 + 2.005.
+       display Ans.
 
        Stop Run.
