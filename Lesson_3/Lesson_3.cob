bb2194366d6fd2b95508829c58fb57af00138e9f
user <user@NOTER023.tkh-staff.local>
2025-05-30 09:31:56 +0900

    Lesson 3 Part 2

--- Lesson_3/Lesson_3.cob
+++ Lesson_3/Lesson_3.cob
@@ -29,5 +29,36 @@
            DISPLAY "You can not vote." 
        END-IF.
 
+       IF Age LESS THAN 5 THEN
+           DISPLAY "Stay Home"
+       END-IF.
+       IF Age = 5 THEN
+           DISPLAY "Go to Kindergarten"
+       END-IF.
+       IF Age > 5 AND Age < 18 THEN
+           COMPUTE Grade = Age - 5
+           DISPLAY "Go to Grade " Grade
+       END-IF.
+       IF Age GREATER THAN OR EQUAL TO 18 THEN
+           DISPLAY "Go to college"
+       END-IF.
+
+       IF Score IS PassingScore THEN
+           DISPLAY "You Passed"
+       ELSE
+           DISPLAY "You Failed"
+       END-IF.
+
+       IF Score IS NOT NUMERIC THEN
+           DISPLAY "Not a Number"
+       END-IF.
+
+       IF Age > 18 THEN
+           SET CanVote TO TRUE
+       ELSE
+           SET CantVote TO TRUE
+       END-IF.
+
+       DISPLAY "Vote: " CanVoteFlag.
        
        STOP RUN.
